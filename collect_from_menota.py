8b6664a97d5e831fd1b7aacf19f1959aaef362fd
Stephen DeGrace <stephenbendl@gmail.com>
2024-02-08 11:34:33 -0500

    Adjust title to filename substitutions, and search through all subelements of for text.

--- collect_from_menota.py
+++ collect_from_menota.py
@@ -15,7 +15,8 @@ def get_tree(path):
 
 with open('./data/menota.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    next(spamreader)
+    for i in range(1):
+        next(spamreader)
     for row in spamreader:
         tree = get_tree(row[-1])
         pages = tree.find('./{http://www.tei-c.org/ns/1.0}text').find('./{http://www.tei-c.org/ns/1.0}body').findall('.//{http://www.tei-c.org/ns/1.0}p')
@@ -23,20 +24,21 @@ with open('./data/menota.csv', newline='') as csvfile:
         dipl = ''
         norm = ''
         title = tree.find('{http://www.tei-c.org/ns/1.0}teiHeader').find('{http://www.tei-c.org/ns/1.0}fileDesc').find('{http://www.tei-c.org/ns/1.0}titleStmt').find('{http://www.tei-c.org/ns/1.0}title').text
-        title = re.sub(r'(:|/|\\|\)|\()', '-', title).replace(' ', '_').replace(':', '')
+        title = re.sub(r'(:|/|\\|\)|\(|,|\.)+', '-', title)
+        title = re.sub(r'\s+', '_', title).replace(' ', '_').replace(':', '').replace('_-_', '-')
         print(title)
         for page in pages:
             # print('\tPage '+page.text)
             for e in page.findall('*'):
                 f = e.find('.//{http://www.menota.org/ns/1.0}facs')
-                if f is not None and f.text is not None:
-                    facs += f.text + ' '
+                if f is not None and f.itertext() is not None:
+                    facs += ''.join(f.itertext()) + ' '
                 d = e.find('.//{http://www.menota.org/ns/1.0}dipl')
-                if d is not None and d.text is not None:
-                    dipl += d.text + ' '
+                if d is not None and d.itertext() is not None:
+                    dipl += ''.join(d.itertext()) + ' '
                 n = e.find('.//{http://www.menota.org/ns/1.0}norm')
-                if n is not None and n.text is not None:
-                    norm += n.text + ' '
+                if n is not None and n.itertext() is not None:
+                    norm += ''.join(n.itertext()) + ' '
 
         if len(facs):
             with open(f'./data/out/{title}_facs.txt', 'w', encoding='utf8') as f:
